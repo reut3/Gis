c8c2c4bd5efd1f55e5dc2e8fc21760f00a582e74
reut <reut@Lenovo-PC>
2017-11-30 14:46:40 +0200

    update

--- src/MyPack/Main.java
+++ src/MyPack/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		CsvFile.readCSV("C:\\Users\\reut\\git\\matala4\\inputFile");
-		KmlFile.readCSV("C:\\Users\\reut\\git\\matala4\finalFile.csv");
+		CsvFile.readCSV("C:\\Users\\reut\\workspace\\matala\\inputFile");
+		KmlFile.readCSV("C:\\Users\\reut\\workspace\\matala\\finalFile.csv");
 		
 		
 	}
